c634cd604f20b6229de2573600ffafe751bdf60b
mfbevan <matthewbevan00@gmail.com>
2022-11-21 09:29:30 +1000

    chore: 🔥 remove dummy data

--- apps/client/components/leaderboard/Leaderboard.tsx
+++ apps/client/components/leaderboard/Leaderboard.tsx
@@ -88,36 +88,3 @@ const LeaderboardContainer = chakra(DefaultContainer, {
     maxW: "600px",
   },
 });
-
-const dummyData = [
-  {
-    entityName: "Lido",
-    entityLogo: "lido",
-    totalBlocks: 10000,
-    censoredBlocks: 9000,
-  },
-  {
-    entityName: "Coinbase",
-    entityLogo: "coinbase",
-    totalBlocks: 9000,
-    censoredBlocks: 8000,
-  },
-  {
-    entityName: "Kraken",
-    entityLogo: "kraken",
-    totalBlocks: 8000,
-    censoredBlocks: 7000,
-  },
-  {
-    entityName: "Binance",
-    entityLogo: "binance",
-    totalBlocks: 7000,
-    censoredBlocks: 6000,
-  },
-  {
-    entityName: "Staked.us",
-    entityLogo: "staked.us",
-    totalBlocks: 6000,
-    censoredBlocks: 5000,
-  },
-];
